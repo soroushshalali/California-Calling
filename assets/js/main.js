ca94d3273ce1ec2f14a8473111716f8e29f4677a
soroushshalali <soroushshalali@gmail.com>
2020-03-01 14:11:09 +0100

    v0.1

---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+function fun(){
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo2").style.display="none";
+    document.getElementById("photo3").style.display="none";
+    document.getElementById("photo4").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+
+function showfoto1(){
+    document.getElementById("btn-foto1").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto2").style.backgroundColor="#fff";
+    document.getElementById("btn-foto3").style.backgroundColor="#fff";
+    document.getElementById("btn-foto4").style.backgroundColor="#fff";
+    document.getElementById("btn-foto5").style.backgroundColor="#fff";
+    document.getElementById("btn-foto6").style.backgroundColor="#fff";
+    document.getElementById("btn-foto7").style.backgroundColor="#fff";
+    document.getElementById("photo1").style.display="block";
+    document.getElementById("photo2").style.display="none";
+    document.getElementById("photo3").style.display="none";
+    document.getElementById("photo4").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+function showfoto2(){
+    document.getElementById("btn-foto2").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto1").style.backgroundColor="#fff";
+    document.getElementById("btn-foto3").style.backgroundColor="#fff";
+    document.getElementById("btn-foto4").style.backgroundColor="#fff";
+    document.getElementById("btn-foto5").style.backgroundColor="#fff";
+    document.getElementById("btn-foto6").style.backgroundColor="#fff";
+    document.getElementById("btn-foto7").style.backgroundColor="#fff";
+    document.getElementById("photo2").style.display="block";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo3").style.display="none";
+    document.getElementById("photo4").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+function showfoto3(){
+    document.getElementById("btn-foto3").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto2").style.backgroundColor="#fff";
+    document.getElementById("btn-foto1").style.backgroundColor="#fff";
+    document.getElementById("btn-foto4").style.backgroundColor="#fff";
+    document.getElementById("btn-foto5").style.backgroundColor="#fff";
+    document.getElementById("btn-foto6").style.backgroundColor="#fff";
+    document.getElementById("btn-foto7").style.backgroundColor="#fff";
+    document.getElementById("photo3").style.display="block";
+    document.getElementById("photo2").style.display="none";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo4").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+function showfoto4(){
+    document.getElementById("btn-foto4").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto2").style.backgroundColor="#fff";
+    document.getElementById("btn-foto3").style.backgroundColor="#fff";
+    document.getElementById("btn-foto1").style.backgroundColor="#fff";
+    document.getElementById("btn-foto5").style.backgroundColor="#fff";
+    document.getElementById("btn-foto6").style.backgroundColor="#fff";
+    document.getElementById("btn-foto7").style.backgroundColor="#fff";
+    document.getElementById("photo4").style.display="block";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo3").style.display="none";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+function showfoto5(){
+    document.getElementById("btn-foto5").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto2").style.backgroundColor="#fff";
+    document.getElementById("btn-foto3").style.backgroundColor="#fff";
+    document.getElementById("btn-foto4").style.backgroundColor="#fff";
+    document.getElementById("btn-foto1").style.backgroundColor="#fff";
+    document.getElementById("btn-foto6").style.backgroundColor="#fff";
+    document.getElementById("btn-foto7").style.backgroundColor="#fff";
+    document.getElementById("photo5").style.display="block";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo3").style.display="none";
+    document.getElementById("photo4").style.display="none";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+function showfoto6(){
+    document.getElementById("btn-foto6").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto2").style.backgroundColor="#fff";
+    document.getElementById("btn-foto3").style.backgroundColor="#fff";
+    document.getElementById("btn-foto4").style.backgroundColor="#fff";
+    document.getElementById("btn-foto5").style.backgroundColor="#fff";
+    document.getElementById("btn-foto1").style.backgroundColor="#fff";
+    document.getElementById("btn-foto7").style.backgroundColor="#fff";
+    document.getElementById("photo6").style.display="block";
+    document.getElementById("photo2").style.display="none";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo4").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo7").style.display="none";
+}
+function showfoto7(){
+    document.getElementById("btn-foto7").style.backgroundColor="rgb(23, 189, 23)";
+    document.getElementById("btn-foto2").style.backgroundColor="#fff";
+    document.getElementById("btn-foto3").style.backgroundColor="#fff";
+    document.getElementById("btn-foto4").style.backgroundColor="#fff";
+    document.getElementById("btn-foto5").style.backgroundColor="#fff";
+    document.getElementById("btn-foto6").style.backgroundColor="#fff";
+    document.getElementById("btn-foto1").style.backgroundColor="#fff";
+    document.getElementById("photo7").style.display="block";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo3").style.display="none";
+    document.getElementById("photo1").style.display="none";
+    document.getElementById("photo5").style.display="none";
+    document.getElementById("photo6").style.display="none";
+    document.getElementById("photo1").style.display="none";
+}
+
+
+
